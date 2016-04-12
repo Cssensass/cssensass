73abd49d60a5e6847f00a21ed475c5f153c43bdd
alerousseau <alerousseau@enseirb-matmeca.fr>
2016-04-12 14:12:21 +0200

    modified:   context.js 	modified:   testContext.js

--- testContext.js
+++ testContext.js
@@ -1,10 +1,9 @@
-<<<<<<< HEAD
 #!/usr/bin/env node
 
 
 var CSSParser = require("css-js");
 var fs = require("fs");
-var matrixTest = new (require('./../context'))();
+var matrixTest = new (require('./context'))();
 
 var config = {};
 config.ver = "3.0";
@@ -30,27 +29,3 @@ for (i=0; i<cssContent.rulesets.length; i++){
 }
 
 matrixTest.printMatrixElement();
-=======
-var matrix = new (require('./context'))();
-
-
-var testContext = new Context();
-
-for (i=0; i<cssContent.rulesets.length; i++){
-	testContext.addEntity(cssContent.rulesets[i].selector);	
-}
-
-
-for (i=0; i<cssContent.rulesets.length; i++){
-	testContext.addAttribute(cssContent.rulesets[i].declaration);	
-}
-
-
-for (i=0; i<cssContent.rulesets.length; i++){
-	testContext.addRelation(cssContent.rulesets[i].selector, cssContent.rulesets[i].declaration);	
-}
-
-console.log(testContext);
-
-testContext.printMatrixElement();
->>>>>>> e1e854482ecbee0419e9cae706dfde85f13502b9
