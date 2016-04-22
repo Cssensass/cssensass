017f00dfb5b308e5c585ad846d9d873b936c3965
ajeoual <ajeoual@users.noreply.github.com>
2016-04-23 00:48:06 +0200

    Update testContext.js

--- testContext.js
+++ testContext.js
@@ -4,6 +4,7 @@
 var CSSParser = require("css-js");
 var fs = require("fs");
 var matrixTest = new (require('./context'))();
+//var arbotest = new (require('./arbo'))();
 
 var config = {};
 config.ver = "3.0";
@@ -14,24 +15,28 @@ var cssContent = parser.parse(fs.readFileSync("test.css", "UTF-8"));
 console.log('Hello user, welcome on CssenSass !');
 console.log('Test of context.js :');
 var EntitiesTab = [];
+
 for (i=0; i<cssContent.rulesets.length; i++){
 	matrixTest.addEntity(cssContent.rulesets[i].selector);	
 }
 
 
-for (i=0; i<cssContent.rulesets.length; i++){
-	matrixTest.addAttribute(cssContent.rulesets[i].declaration);	
-}
+	matrixTest.addAttribute(cssContent.rulesets, cssContent.rulesets.length);	
+
 
 
 for (i=0; i<cssContent.rulesets.length; i++){
 	matrixTest.addRelation(cssContent.rulesets[i].selector, cssContent.rulesets[i].declaration);	
 }
 
+
+/*arbotest.fonctionF(matrixTest.attributes, matrixTest.relations, matrixTest.entities);
+
+console.log(arbotest.listTmpFinal);*/
 //console.log(matrixTest);
 console.log('\n');
-console.log(matrixTest.relations[2][1][1].value);
+console.log(matrixTest.attributes);
 console.log('\n');
 //matrixTest.printMatrixElement();
-console.log(matrixTest.reverseRelations[4][1]);
+console.log(matrixTest.relations);
 console.log('\n');
