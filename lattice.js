c52e9fec9388c892d39764272e8a131ae0e00965
alerousseau <alerousseau@enseirb-matmeca.fr>
2016-05-11 11:14:01 +0200

    new file:   lattice.js 	modified:   concept.js completing concept

---
+++ lattice.js
@@ -0,0 +1,18 @@
+
+function lattice(){
+
+	this.topologicalOrder = function(){
+		var result = [];
+		var visitedNodes = [];
+
+	}
+
+	this.topologicalOrderAux = function(node, visitedNodes, topologicalOrder){
+		var parent = [];
+		for(var i=0; i<visitedNodes.length; i++){
+			if(!(visitedNodes[i] == node)){
+				for()
+			}
+		}
+	}
+}
