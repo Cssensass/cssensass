a1be9da026456922cbd4e33958d31627b9ef96f6
alerousseau <alerousseau@enseirb-matmeca.fr>
2016-05-13 10:35:29 +0200

    modified:   node.js

--- node.js
+++ node.js
@@ -4,4 +4,28 @@ function Node(identifier, entities, attributes){
 	this.parents = [];
 	this.children = [];
 
+	this.getEntities = function (){
+		return this.entities;
+	}
+
+	this.getAttributes = function(){
+		return this.attributes;
+	}
+
+	this.getSimplifiedAttributes = function(){
+		var result = [];
+		for(i=0; i<attributes.length; i++){
+			var leafAttribute = true;
+			for(j=0; j<parents.length; j++){
+				if(this.parents[j][0] == attributes[i]){
+					leafAttribute = false;
+				}
+			}
+			if(leafAttribute){
+				result.push(attributes[i]);
+			}
+		}
+		return result
+	}
+
 }
