1d1c0be6ac1615519ee557faf9f8037d0689a29c
alerousseau <alerousseau@enseirb-matmeca.fr>
2016-05-16 11:47:23 +0200

    modified:   CssToSsl.js

--- CssToSsl.js
+++ CssToSsl.js
@@ -3,17 +3,26 @@ var cssToSsl = function cssToSsl(){
 	this.buildContext = function(cssContent){
 		var context = new (require('./context'))();
 
-		for (i=0; i<cssContent.rulesets.length; i++){
+		for (var i=0; i<cssContent.rulesets.length; i++){
 			context.addEntity(cssContent.rulesets[i].selector);	
 		}
 		
 		context.addAttribute(cssContent.rulesets, cssContent.rulesets.length);	
 		
-		for (i=0; i<cssContent.rulesets.length; i++){
+		for (var i=0; i<cssContent.rulesets.length; i++){
 			context.addRelation(cssContent.rulesets[i].selector, cssContent.rulesets[i].declaration);	
 		}
 
 		return context;
+	}
+
+	this.generateLatticeFromConcepts = function(concepts){
+		var nodes = [];
+		for(var i = 0; i<concepts.length; i++){
+			var entities = concepts[i].getSimplifiedExtents();
+			// Aaaand the fun is coming...
+		}
+
 	}
 
 
